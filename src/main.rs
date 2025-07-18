b000cf05fede2a3b91f6f521ac19d5dc379783d9
MattPlayGamez <62027538+MattPlayGamez@users.noreply.github.com>
2025-07-18 22:32:54 +0200

    updated some things to allow images

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,29 @@ use std::env::args;
 use std::process::{Command, Stdio};
 use std::process::exit;
 
+/// The main entry point of the application.
+///
+/// This function is called when the application is started. It checks the
+/// command line arguments and prints a usage message if there are too few
+/// arguments or if the user requested help. Otherwise, it copies the public
+/// SSH key to the specified location.
+///
+/// The public key is read from the file `id_rsa.pub` in the user's
+/// home directory. The private key is not used.
+///
+/// The public key is appended to the file `authorized_keys` in the user's
+/// home directory on the remote machine. If the file does not exist, it is
+/// created.
+///
+/// The user is asked to enter their password when connecting to the remote
+/// machine. The password is not stored anywhere.
+///
+/// # Example
+///
+/// To copy the public key to the user `matt` on the host `192.168.0.2`, run
+/// the following command:
+///
+///
 fn main() {
     let args: Vec<String> = args().collect();
     if args.len() < 2 {
